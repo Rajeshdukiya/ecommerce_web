c1a2d09dbcdb6e3bee5bcd08d681eeeabe53e355
RAJESHDUKIYA <rajeshdukiya2000@gmail.com>
2023-11-19 08:13:56 +0530

    create_cart_modal_page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import myContext from '../../context/data/myContext'
 import Layout from '../../components/layout/Layout'
+import Modal from '../../components/modal/Modal'
 
 const Cart = () => {
   const context = useContext(myContext)
@@ -46,13 +47,7 @@ const Cart = () => {
                 <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>₹200</p>
               </div>
             </div>
-            {/* <Modal  /> */}
-            <button
-              type="button"
-              className="w-full  bg-violet-600 py-2 text-center rounded-lg text-white font-bold "
-            >
-              Buy Now
-            </button>
+            <Modal/> 
           </div>
         </div>
       </div>
